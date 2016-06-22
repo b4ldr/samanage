b0e09aee847b64e2f942391852abd6d9a5d94684
b4ldr <github@johnbond.org>
2016-06-22 14:06:14 +0100

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='samanage',
+      version='1.0',
+      description='Library to interface with samanage',
+      author='John Bond',
+      author_email='pypi@johnbond.org',
+      url='https://github.com/b4ldr/samanage',
+      license='BSD',
+      packages=['samanage'],
+     )
